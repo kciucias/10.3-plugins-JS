98ca8513906a88832157bce9f26a6b1c38f342ae
Katarzyna Ciucias <kasia.ciucias@gmail.com>
2018-07-11 23:45:23 +0100

    Add map

--- js/script.js
+++ js/script.js
@@ -49,4 +49,26 @@ for (var i = 0; i < anchors.length; i++) {
     });
 }
 
+
+// MAPS
+    window.initMap = function() {
+    
+      // Zapisujemy w zmiennej obiekt zawierający współrzędne geograficzne.
+      var uluru = {lat: -25.363, lng: 131.044};
+      
+      // W zmiennej map zapisujemy nową instancję obiektu Map. 
+      var map = new google.maps.Map(document.getElementById('map'), {
+        // Podajemy opcje mapy, np. zoom i punkt wycentrowania mapy.
+        zoom: 4,
+        center: uluru
+      });
+      
+      // Definiujemy marker jako nową instancję obiektu Marker.
+      var marker = new google.maps.Marker({
+        // I podajemy opcje tego markera, np. na której mapie ma być dodany oraz jakie są jego współrzędne. 
+        position: uluru,
+        map: map
+      }); 
+    };
+
 })(); 
